01762ecbf52d81b6ca81d56991d48cd58fde41b9
Luiz Fernando Amador Loiola Pereira <73619308+luizAmador2003@users.noreply.github.com>
2022-08-27 20:29:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+<title>Exemplo PHP</title>
+</head>
+<body>
+
+<?php
+ini_set("display_errors", 1);
+header('Content-Type: text/html; charset=iso-8859-1');
+
+$servername = "54.234.153.24";
+$username = "root";
+$password = "Senha123";
+$database = "meubanco";
+
+$link = new mysqli($servername, $username, $password, $database);
+
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$valor_rand1 =  rand(1, 999);
+$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+$host_name = gethostname();
+
+
+$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+
+if ($link->query($query) === TRUE) {
+  echo "successfully";
+} else {
+  echo "Error: " . $link->error;
+}
+
+?>
+</body>
+</html>
